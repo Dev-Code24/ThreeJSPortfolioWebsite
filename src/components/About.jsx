1bd143c78785f16c89d4feddd3a4440987abee50
Dev Code <hellodev_code@outlook.com>
2023-03-23 21:10:52 +0530

    react-tilt to react-parallax-tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
-import Tilt from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 
 const ServiceCard = ({ index, title, icon, isMobile }) => {
   return isMobile ? (
